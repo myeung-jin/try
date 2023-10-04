027dafccb04b2b5501f598740bae0101d99698c1
myeung-jin <146822281+myeung-jin@users.noreply.github.com>
2023-10-04 15:30:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import streamlit as st
 st.write('# Hello World')
 st.write('# 이제 스트림릿으로 인터넷 페이지 공유시작')
+
+view = [100,150,30]
+st.write('# Youtube view')
+view
+st.write('# bar chart')
+st.bar_chart(view)
+import pandas as pd
+sview = pd.Series(view)
+
